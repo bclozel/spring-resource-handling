136ac451f35de6174549e2d9aabc154aa177e37f
Brian Clozel <bclozel@pivotal.io>
2014-12-30 11:46:44 +0100

    Update to Spring Boot 1.2.0 and Spring 4.1.3
    * use auto-configured internal view resolver for JSPs
    * remove custom Groovy Template resolver (auto-configured by Boot)
    * use Boot configuration keys
    * use `@SpringBootApplication` annotation

--- server/src/main/java/org/springframework/samples/resources/Application.java
+++ server/src/main/java/org/springframework/samples/resources/Application.java
@@ -1,20 +1,16 @@
 package org.springframework.samples.resources;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(new Object[] {Application.class}, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
--- server/src/main/java/org/springframework/samples/resources/WebConfig.java
+++ server/src/main/java/org/springframework/samples/resources/WebConfig.java
@@ -1,5 +1,11 @@
 package org.springframework.samples.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.annotation.PostConstruct;
+
 import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +13,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.samples.resources.groovy.GroovyHelperViewResolver;
 import org.springframework.samples.resources.handlebars.ProfileHelper;
 import org.springframework.samples.resources.handlebars.ResourceUrlHelper;
 import org.springframework.util.Assert;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.*;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.groovy.GroovyMarkupConfigurer;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+import org.springframework.web.servlet.view.groovy.GroovyMarkupViewResolver;
 
-
-@EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
+
+	@Autowired
+	private GroovyMarkupViewResolver groovyMarkupViewResolver;
+
+	@Autowired
+	private ResourceUrlProvider urlProvider;
 
 	@Value("${resources.projectroot:}")
 	private String projectRoot;
@@ -49,48 +59,26 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public HandlebarsViewResolver handlebarsViewResolver(ResourceUrlProvider urlProvider) {
+	public HandlebarsViewResolver handlebarsViewResolver() {
 		HandlebarsViewResolver resolver = new HandlebarsViewResolver();
 		resolver.setPrefix("classpath:/handlebars/");
-		resolver.registerHelper("src", new ResourceUrlHelper(urlProvider));
+		resolver.registerHelper("src", new ResourceUrlHelper(this.urlProvider));
 		resolver.registerHelper(ProfileHelper.NAME, new ProfileHelper(this.env.getActiveProfiles()));
 		resolver.setCache(!this.env.acceptsProfiles("development"));
 		resolver.setFailOnMissingFile(false);
 		return resolver;
 	}
 
-	@Bean
-	public GroovyHelperViewResolver groovyViewResolver(ResourceUrlProvider urlProvider) {
-		GroovyHelperViewResolver resolver = new GroovyHelperViewResolver();
-		resolver.setSuffix(".tpl");
-		resolver.addTemplateHelper("linkTo", s -> urlProvider.getForLookupPath((String) s));
-		return resolver;
-	}
-
-	@Bean
-	public GroovyMarkupConfigurer groovyTemplateConfigurer() {
-
-		GroovyMarkupConfigurer configurer = new GroovyMarkupConfigurer();
-		configurer.setResourceLoaderPath("classpath:groovy/");
-		configurer.setAutoIndent(true);
-		configurer.setAutoNewLine(true);
-		configurer.setCacheTemplates(false);
-		return configurer;
-	}
-
-	@Bean
-	public InternalResourceViewResolver defaultViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
+	@PostConstruct
+	public void registerGroovyTemplateHelpers() {
+		Map<String, Function> groovyTemplateHelpers = new HashMap<>();
+		groovyTemplateHelpers.put("linkTo", s -> this.urlProvider.getForLookupPath((String) s));
+		this.groovyMarkupViewResolver.setAttributesMap(groovyTemplateHelpers);
 	}
 
 	@Bean
 	public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-		ResourceUrlEncodingFilter filter = new ResourceUrlEncodingFilter();
-
-		return filter;
+		return new ResourceUrlEncodingFilter();
 	}
 
 	@Override
